b55a3be91268069417df6e4cc7d1e1f1dee6eb58
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-18 20:08:25 -0400

    Agregando la configuracion del Hilt

---
+++ app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/RuthApp.kt
@@ -0,0 +1,7 @@
+package edu.ucne.composedemo.ruthcasilla_p1_ap2
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RuthApp {
+}
---
+++ app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/data/AppModule.kt
@@ -0,0 +1,10 @@
+package edu.ucne.composedemo.ruthcasilla_p1_ap2.data
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+}

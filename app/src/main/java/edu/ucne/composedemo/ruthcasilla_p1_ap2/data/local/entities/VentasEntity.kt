a9e268071e93fd96ccc95b1806c31c16569a23cf
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-25 18:22:14 -0400

    Agregando la entidad

---
+++ app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/data/local/entities/VentasEntity.kt
@@ -0,0 +1,16 @@
+package edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Ventas")
+data class VentasEntity (
+    @PrimaryKey(autoGenerate = true)
+    val ventaId: Int? = null,
+    val nombreCliente: String?,
+    val galones: Double,
+    val descuentoGalon: Double,
+    val precio: Double,
+    val totalDescontado: Double,
+    val total: Double
+)

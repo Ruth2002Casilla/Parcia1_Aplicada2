81792d670dcd07eba93520290a899001a876fc7f
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-25 20:54:58 -0400

    Actualizando el VentaRepository

--- app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/data/local/repository/VentaRepository.kt
+++ app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/data/local/repository/VentaRepository.kt
@@ -1,15 +1,15 @@
 package edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.repository
 
 import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.dao.VentasDao
-import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.entities.VentasEntity
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.entities.ventasEntity
 import javax.inject.Inject
 
 class VentaRepository @Inject constructor(
     private val ventasDao: VentasDao
 ){
-    suspend fun save(venta: VentasEntity) = ventasDao.save(venta)
+    suspend fun save(venta: ventasEntity) = ventasDao.save(venta)
 
-    suspend fun delete(venta: VentasEntity) = ventasDao.delete(venta)
+    suspend fun delete(venta: ventasEntity) = ventasDao.delete(venta)
 
     suspend fun getVenta(id: Int) = ventasDao.find(id)
 

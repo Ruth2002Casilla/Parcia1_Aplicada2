fd2a7485959a4bd5ffbb03c606ca4ffd100b0f7f
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-25 20:55:15 -0400

    Actualizando el VentaDb

--- app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/data/local/database/VentaDb.kt
+++ app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/data/local/database/VentaDb.kt
@@ -3,11 +3,11 @@ package edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.dao.VentasDao
-import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.entities.VentasEntity
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.entities.ventasEntity
 
 @Database(
     entities = [
-        VentasEntity::class
+        ventasEntity::class
     ],
     version = 1,
     exportSchema = false

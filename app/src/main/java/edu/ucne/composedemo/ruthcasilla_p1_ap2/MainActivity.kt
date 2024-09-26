1665be119b6e527c0982f0854e443906e147222a
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-25 20:55:26 -0400

    Actualizando el MainActivity

--- app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/MainActivity.kt
@@ -4,20 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.database.VentaDb
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.presentation.navigation.NavigationNavHost
 import edu.ucne.composedemo.ruthcasilla_p1_ap2.ui.theme.RuthCasilla_P1_Ap2Theme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var ventaDb: VentaDb
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ventaDb = Room.databaseBuilder(
+            applicationContext,
+            VentaDb::class.java,
+            "Ventas.db"
+        ).build()
+
         enableEdgeToEdge()
+
+        val ventasDao = ventaDb.VentasDao()
+
         setContent {
             RuthCasilla_P1_Ap2Theme {
+                val navController = rememberNavController()
 
-                }
+                NavigationNavHost(
+                    navHostController = navController,
+                    VentaLista = ventasDao.getAll(),
+                    ventasDao = ventasDao,
+                )
             }
         }
     }
-
-
+}

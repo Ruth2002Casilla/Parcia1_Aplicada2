c96ede35210ec6c865e06538ef0852a10278addf
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-25 20:54:21 -0400

    Actualizando el RuthApp

--- app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/RuthApp.kt
+++ app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/RuthApp.kt
@@ -1,7 +1,9 @@
 package edu.ucne.composedemo.ruthcasilla_p1_ap2
 
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RuthApp {
+class RuthApp : Application(){
+
 }

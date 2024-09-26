8b3171d27f5ce034e009bd855f855866ffb6e600
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-25 20:55:47 -0400

    Actualizando el NavigationNavHost

--- app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/presentation/navigation/NavigationNavHost.kt
+++ app/src/main/java/edu/ucne/composedemo/ruthcasilla_p1_ap2/presentation/navigation/NavigationNavHost.kt
@@ -2,50 +2,113 @@ package edu.ucne.composedemo.ruthcasilla_p1_ap2.presentation.navigation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import edu.ucne.composedemo.ruthcasilla_p1_ap2.ui.theme.Purple80
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.dao.VentasDao
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.data.local.entities.ventasEntity
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.presentation.component.NavigationDrawer
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.presentation.ventas.DeleteVentaScreen
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.presentation.ventas.EditarVentasScreen
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.presentation.ventas.ListVentasScreen
+import edu.ucne.composedemo.ruthcasilla_p1_ap2.presentation.ventas.VentaScreen
+import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun NavigationNavHost(
-    navHostController: NavHostController)
-{
+    navHostController: NavHostController,
+    VentaLista: Flow<List<ventasEntity>>,
+    ventasDao: VentasDao
+){
     val isDrawerVisible = remember { mutableStateOf(false) }
+
     Box(modifier = Modifier.fillMaxSize()) {
-        NavHost(navController = navHostController, startDestination = Screen.ListScreen::class.java.simpleName) {
+        NavHost(navController = navHostController, startDestination = Screen.ventaListScreen) {
 
-            composable<Screen.ListScreen> {
-                Button(
-                    onClick = {
-                        navHostController.navigate(Screen.RegistroScreen::class.java.simpleName)
+            //Metas
+            composable<Screen.ventaListScreen> {
+                ListVentasScreen(
+                    onDrawerToggle = {
+                        isDrawerVisible.value = !isDrawerVisible.value
                     },
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Purple80,
-                    ),
-                    shape = RoundedCornerShape(10.dp)
-                    ) {
+                    goToVenta = {
+                        navHostController.navigate(Screen.ventaListScreen)
+                    },
+                    createVenta = {
+                        navHostController.navigate(Screen.createVentaScreen)
+                    },
+                    editVenta = {
+                        navHostController.navigate(Screen.editarVentaScreen(it))
+                    },
+                    deleteVenta = {
+                        navHostController.navigate(Screen.eliminarVentaScreen(it))
+                    }
+                )
+            }
 
-                    Text(text = "Registro")
+            composable<Screen.createVentaScreen> {
+                VentaScreen(
+                    onDrawerToggle = {
+                        isDrawerVisible.value = !isDrawerVisible.value
+                    },
+                    goToVenta = {
+                        navHostController.navigate(Screen.ventaListScreen)
+                    }
+                )
+            }
+            composable<Screen.editarVentaScreen> { backStackEntry ->
+                val ventaId = backStackEntry.arguments?.getInt("ventaId")
+                if (ventaId != null) {
+                    EditarVentasScreen(
+                        ventaId = ventaId,
+                        onDrawerToggle = {
+                            isDrawerVisible.value = !isDrawerVisible.value
+                        },
+                        goToVenta = {
+                            navHostController.navigate(Screen.ventaListScreen) {
+                                popUpTo(Screen.ventaListScreen) { inclusive = true }
+                            }
+                        }
+                    )
                 }
             }
 
-            composable<Screen.RegistroScreen> {}
+
+            composable<Screen.eliminarVentaScreen> { backStackEntry ->
+                val ventaId = backStackEntry.arguments?.getInt("ventaId")
+                if (ventaId != null) {
+                    DeleteVentaScreen(
+                        ventaId = ventaId,
+                        onDrawerToggle = {
+                            isDrawerVisible.value = !isDrawerVisible.value
+                        },
+                        goToVenta = {
+                            navHostController.navigate(Screen.ventaListScreen) {
+                                popUpTo(Screen.ventaListScreen) { inclusive = true }
+                            }
+                        }
+                    )
+                }
+            }
 
         }
-        
+
+        NavigationDrawer(
+            isVisible = isDrawerVisible.value,
+            onItemClick = { itemTitle ->
+                when (itemTitle) {
+                    "Home" -> navHostController.navigate(Screen.ventaListScreen)
+                }
+                isDrawerVisible.value = false
+            },
+            onClose = {
+                isDrawerVisible.value = false
+            }
+        )
+
     }
 }
-
